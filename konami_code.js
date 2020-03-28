06875e8cf9067c952c08571a6a132e12a9ac7724
Jackson Chen <jacksonchen171@gmail.com>
2020-03-28 14:40:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', (event) => {
+    const key = e.key
+    let index = 0
+    if (key === codes[index]) {
+      index += 1
+      if (index===codes.length){
+        alert("Nice!")
+      }
+    }
+    else {
+      index = 0
+    }
+  })
 }
